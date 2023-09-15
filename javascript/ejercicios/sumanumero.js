2ad481cf168bfe3a3c86b44acffcda89df2df29b
Juan Rangel de Prada <juansolobolos@gmail.com>
2023-09-15 20:22:35 +0200

    tercer commit

--- javascript/ejercicios/sumanumero.js
+++ javascript/ejercicios/sumanumero.js
@@ -1,12 +1,14 @@
-
+/**Carga ek modulo. Funcion de node.js para cargar modulos */
 const prompt = require('prompt-sync')();
 
-
+/**Lee un par de numeros */
 const numero1 = Number(prompt('numero 1 = '));
 const numero2 = Number(prompt('numero 2 = '));
 
+/**Muestra el resultado */
+console.log(numero1+numero2);
+
+/**Bucle for */
 for(n = 1; n < 10; n++){
     console.log(n);
 }
-
-console.log(numero1+numero2);
