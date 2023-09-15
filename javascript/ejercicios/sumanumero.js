303c2714b473295f5a925a7a3306dd557efc0b0b
Juan Rangel de Prada <juansolobolos@gmail.com>
2023-09-15 20:11:42 +0200

    egundo commit

---
+++ javascript/ejercicios/sumanumero.js
@@ -0,0 +1,12 @@
+
+const prompt = require('prompt-sync')();
+
+
+const numero1 = Number(prompt('numero 1 = '));
+const numero2 = Number(prompt('numero 2 = '));
+
+for(n = 1; n < 10; n++){
+    console.log(n);
+}
+
+console.log(numero1+numero2);
